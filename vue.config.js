d97eb2a238a62e1eb43de8c0711a73e393b134a3
杨伏 <1204914685@qq.com>
2024-08-05 15:08:40 +0800

    feat: add

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    disableHostCheck: true,
+    host: '0.0.0.0', // 或者设置为你的服务器的实际 IP 地址
+    port: 8080
+  },
   transpileDependencies: true
 })
